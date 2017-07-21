a5bf35580404dc598082d618c3b8060c53907d56
epicharri <harri.kahkonen3@gmail.com>
2017-07-21 17:59:35 +0300

    power, last-element

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,10 +1,24 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+      (cond
+        (zero? base)
+          0
+        (= exp 0)
+          1
+        :else
+          (let [helper (fn [acc n]
+                       (if (= n 1)
+                         acc
+                         (recur (* acc base) (dec n))))]
+           (helper base exp))))
 
 (defn last-element [a-seq]
-  ":(")
+      (let [helper (fn [acc the-seq]
+                       (if (empty? the-seq)
+                         acc
+                         (recur (first the-seq) (rest the-seq))))]
+           (helper (first a-seq) (rest a-seq))))
 
 (defn seq= [seq1 seq2]
   ":(")
